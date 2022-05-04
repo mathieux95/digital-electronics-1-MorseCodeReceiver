bbb5ae6c2ae07944364cbf9765f3b3b7521f0d0c
patrikhor <99591251+patrikhor@users.noreply.github.com>
2022-05-04 15:00:36 +0200

    Add files via upload

---
+++ sources/0-final/decoder2.vhd
@@ -0,0 +1,107 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 25.04.2022 18:43:53
+-- Design Name: 
+-- Module Name: decoder - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity decoder2 is
+    Port ( clk        : in  std_logic;
+           reset      : in  std_logic;
+           seconds_h_i : in STD_LOGIC_VECTOR (3 - 1 downto 0);
+           seconds_l_i : in STD_LOGIC_VECTOR (4 - 1 downto 0);
+           hundredths_h_i : in STD_LOGIC_VECTOR (4 - 1 downto 0);
+           hundredths_l_i : in STD_LOGIC_VECTOR (4 - 1 downto 0);
+           rise_i : in std_logic;
+           fall_i : in std_logic;
+           char_o : out std_logic;
+           word_o : out std_logic;
+           en_o : out std_logic;
+           rst_o : out std_logic);
+
+end decoder2;
+
+architecture Behavioral of decoder2 is
+    
+        -- Internal clock enable
+    signal s_en    : std_logic;
+    
+begin
+    --------------------------------------------------------------------
+    -- Instance (copy) of clock_enable entity generates an enable pulse
+    -- every 10 ms (100 Hz).
+
+    -- JUST FOR SHORTER/FASTER SIMULATION
+   -- s_en <= '1';
+--    clk_en0 : entity work.clock_enable
+--        generic map(
+--            g_MAX =>  100000      -- 10 ms / (1/100 MHz) = g_MAX
+--        )
+--        port map(
+--            ce_o => s_en,
+--            clk => clk,
+--            reset => reset
+      
+--        );
+        
+     p_decoder_cnt : process(clk)
+    begin
+        if rising_edge(clk) then
+
+            if (reset = '1') then           -- Synchronous reset
+                char_o <= '0';
+                word_o <= '0';  
+                en_o <= '0';                 
+            elsif(rise_i = '1')then
+                --checking if its end of char -- 3 - 6s    0,2-0,6
+                if(seconds_l_i >= "0001" and seconds_l_i <= "0011") then
+                    char_o <= '1';
+                    en_o <= '1';
+                --checking if its end of word 
+                elsif(seconds_l_i  > "0011") then
+                    word_o <= '1';
+                    en_o <= '1';                 
+                end if;
+            elsif(seconds_h_i = "001") then 
+                word_o <= '1';
+                en_o <= '1'; 
+                rst_o <= '1';
+            --set outputs back to 0      
+            else
+                rst_o <= '0';
+                char_o <= '0';
+                word_o <= '0';
+                en_o <= '0';
+                                                       
+            end if;
+        end if;
+    end process p_decoder_cnt;
+
+end Behavioral;
---
+++ sources/0-final/hex_7seg.vhd
@@ -0,0 +1,172 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/09/2022 03:15:11 PM
+-- Design Name: 
+-- Module Name: hex_7seg - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity hex_7seg is
+    Port ( hex_i : in STD_LOGIC_VECTOR (8 - 1 downto 0);
+           seg_o : out STD_LOGIC_VECTOR (7 - 1 downto 0));
+end hex_7seg;
+
+architecture Behavioral of hex_7seg is
+begin
+
+    --------------------------------------------------------------------
+    -- p_7seg_decoder:
+    -- A combinational process for 7-segment display (Common Anode)
+    -- decoder. Any time "hex_i" is changed, the process is "executed".
+    -- Output pin seg_o(6) controls segment A, seg_o(5) segment B, etc.
+    --       segment A
+    --        | segment B
+    --        |  | segment C
+    --        +-+|  |   ...   segment G
+    --          ||+-+          |
+    --          |||            |
+    -- seg_o = "0000001"-------+
+    --------------------------------------------------------------------
+    p_7seg_decoder : process(hex_i)
+    begin
+        case hex_i is
+            when "00011111" =>
+                seg_o <= "0000001";     -- 0
+
+            when "00101111" =>
+                seg_o <= "1001111";     -- 1
+
+            when "00100111" =>
+                seg_o <= "0010010";     -- 2
+
+            when "00100011" =>
+                seg_o <= "0000110";     -- 3
+
+            when "00100001" =>
+                seg_o <= "1001100";     -- 4
+
+            when "00100000" =>
+                seg_o <= "0100100";     -- 5
+
+            when "00110000" =>
+                seg_o <= "0100000";     -- 6
+
+            when "00111000" =>
+                seg_o <= "0001111";     -- 7
+
+            when "00111100" =>
+                seg_o <= "0000000";     -- 8
+
+            when "00111110" =>
+                seg_o <= "0000100";     -- 9
+
+            when "00000101" =>
+                seg_o <= "0000010";     -- A
+
+            when "00011000" =>
+                seg_o <= "1100000";     -- B
+
+            when "00011010" =>
+                seg_o <= "1110010";     -- C 
+
+            when "00001100" =>
+                seg_o <= "1000010";     -- D 
+
+            when "00000010" =>
+                seg_o <= "0110000";     -- E
+
+	    when "00010010" =>
+                seg_o <= "0111000";     -- F
+
+            when "00001110" =>
+                seg_o <= "0100001";     -- G
+
+            when "00010000" =>
+                seg_o <= "1101000";     -- H
+
+            when "00000100" =>
+                seg_o <= "0111011";     -- I 
+
+            when "00010111" =>
+                seg_o <= "0100111";     -- J
+
+            when "00001101" =>
+                seg_o <= "0101000";     -- K
+
+	    when "00010100" =>
+                seg_o <= "1110001";     -- L
+
+	    when "00000111" =>
+                seg_o <= "0101010";     -- M
+
+            when "00000110" =>
+                seg_o <= "1101010";     -- N
+
+            when "00001111" =>
+                seg_o <= "1100010";     -- O 
+
+            when "00010110" =>
+                seg_o <= "0011000";     -- P
+
+            when "00011101" =>
+                seg_o <= "0001100";     -- Q
+
+	    when "00001010" =>
+                seg_o <= "1111010";     -- R
+
+	    when "00001000" =>
+                seg_o <= "0100101";     -- S
+
+            when "00000011" =>
+                seg_o <= "1110000";     -- T
+
+	    when "00001001" =>
+                seg_o <= "1100011";     -- U
+
+	    when "00010001" =>
+                seg_o <= "1010101";     -- V
+
+	    when "00001011" =>
+                seg_o <= "1010100";     -- W
+
+            when "00011001" =>
+                seg_o <= "1101011";     -- X
+
+	    when "00011011" =>
+                seg_o <= "1000100";     -- Y
+	   
+ 	    when "00011100" =>
+                seg_o <= "0010011";     -- Z
+
+            when others =>
+                seg_o <= "1111111";     --  jinak nesviti
+
+        end case;
+    end process p_7seg_decoder;
+
+end architecture Behavioral;
